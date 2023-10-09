6a89ba2bdda07ca42568c795d06a46e17400ae42
Artem Ulianytskyi <32933673+Ulianytskyi@users.noreply.github.com>
2023-10-09 18:21:09 +0300

    Commit 3

--- scripts/script.js
+++ scripts/script.js
@@ -7,12 +7,14 @@ let unitCoords = [
     {x: 0, y: 300}, {x: 100, y: 300},{x: 200, y: 300},
 ];
 
-function UnitObject(name='Bot', hp=10, damage=3, active=true) {
+function UnitObject(name='Bot', hp=75, damage=55, active=true) {
     this.name = name;
     this.hp = hp;
     this.damage = damage;
     this.active = active;
 }
+
+let hitChance = 80;
 
 function createUnits() {
     let allUnitsArray = [];
@@ -20,15 +22,21 @@ function createUnits() {
         let side = i < 3 ? 'hero' : 'enemy';
         const unitShell = document.createElement('div');
         const unitObject = new UnitObject(side);
+
         unitShell.className = `unit ${side} active`;
         unitShell.dataset.count = i;
         unitShell.innerHTML = `${unitObject.name} ${unitObject.hp}`;
         unitShell.style.top = unitCoords[i].x + 'px';
         unitShell.style.left = unitCoords[i].y + 'px';
+
         gameBoard.appendChild(unitShell);
         allUnitsArray.push(unitObject);
     }
     return allUnitsArray;
+}
+
+function getRandomNumber(value) {
+    return Math.floor(Math.random() * value) + 1;
 }
 
 unitBase = createUnits();
@@ -40,90 +48,133 @@ allUnits.forEach(unit => {
 
 let unitFirst = null;
 let unitSecond = null;
-let turnCounter = 0;
 let noActivity = false;
 let unitHeroAlive = 3;
 let unitEnemyAlive = 3;
 
-function handleUnitClicker(event) {
+let randomSide = getRandomNumber(2) == 1 ? 'hero' : 'enemy';
+let currentTurn = randomSide;
 
-    if (unitFirst === null && 
-        unitSecond === null &&
-        !event.target.classList.contains('inactive') &&
-        !event.target.classList.contains('dead')) {
+// turn side field -------------------------------------------
 
-            unitFirst = event.target;
-            unitFirst.classList.remove('active');
-            unitFirst.classList.add('selected');
+const turnInfoField = document.createElement('div');
+turnInfoField.id = 'turn-info-field';
+gameBoard.appendChild(turnInfoField);
 
-    } else if (unitFirst !== null && 
-        unitSecond === null && 
-        unitFirst.classList[1] != event.target.classList[1] &&
-        !event.target.classList.contains('dead')) {
-
-            unitSecond = event.target;
-            unitSecond.classList.remove('active');
-            unitSecond.classList.add('selected');
-
-            battle(unitFirst, unitSecond);
-
-            unitFirst = null;
-            unitSecond = null;
-
-    } else if (unitFirst == event.target &&
-        !event.target.classList.contains('inactive')) {
-
-            unitFirst.classList.remove('selected');
-            unitFirst.classList.add('active');
-            unitFirst = null;
-
-    } else if (!event.target.classList.contains('inactive') &&
-        !event.target.classList.contains('dead')) {
-
-            unitFirst.classList.remove('selected');
-            unitFirst.classList.add('active');
-            unitFirst = event.target;
-            unitFirst.classList.remove('active');
-            unitFirst.classList.add('selected');
-
+function turnInfoPosition () {
+    if (currentTurn == 'hero') {
+        turnInfoField.style.left = 16 + 'px';
+        turnInfoField.style.color = 'blue';
+        turnInfoField.innerHTML = 'Hero turn';
+    } else {
+        turnInfoField.style.left = 310 + 'px';
+        turnInfoField.style.color = 'red';
+        turnInfoField.innerHTML = 'Enemy turn';
     }
 }
 
+turnInfoPosition ();
+
+// console.log('firts turn:', currentTurn);
+
+function handleUnitClicker(event) {
+    
+    if ((currentTurn == 'hero' && event.target.classList.contains('hero')) ||
+        (currentTurn == 'enemy' && event.target.classList.contains('enemy'))) {
+ 
+            if (unitFirst === null && 
+            unitSecond === null &&
+            !event.target.classList.contains('inactive') &&
+            !event.target.classList.contains('dead')) {
+
+                    unitFirst = event.target;
+                    unitFirst.classList.remove('active');
+                    unitFirst.classList.add('selected');
+
+                    currentTurn = currentTurn === 'hero' ? 'enemy' : 'hero';
+            } else if (unitFirst !== null && 
+                unitSecond === null && 
+                unitFirst.classList[1] != event.target.classList[1] &&
+                !event.target.classList.contains('dead')) {
+
+                    unitSecond = event.target;
+                    unitSecond.classList.remove('active');
+                    unitSecond.classList.add('selected');
+
+                    battle(unitFirst, unitSecond);
+
+                    unitFirst = null;
+                    unitSecond = null;  
+            } else if (unitFirst == event.target &&
+                !event.target.classList.contains('inactive')) {
+
+                    unitFirst.classList.remove('selected');
+                    unitFirst.classList.add('active');
+                    unitFirst = null;
+            } else if (!event.target.classList.contains('inactive') &&
+                !event.target.classList.contains('dead')) {
+
+                    unitFirst.classList.remove('selected');
+                    unitFirst.classList.add('active');
+                    unitFirst = event.target;
+                    unitFirst.classList.remove('active');
+                    unitFirst.classList.add('selected');
+            }
+    }
+}
+
+function turnCheck() {
+    if (document.querySelectorAll('.hero.active').length === 0 &&
+    document.querySelectorAll('.hero.selected').length === 0) {
+        // console.log('No hero who can fight');
+        currentTurn = 'enemy';
+    } else if (document.querySelectorAll('.enemy.active').length === 0 &&
+    document.querySelectorAll('.enemy.selected').length === 0) {
+        // console.log('No enemy who can fight');
+        currentTurn = 'hero'; 
+    }
+    turnInfoPosition ();
+}
+    
 function battle(u1, u2) {
     setTimeout( function() {
         calculateDamage(u1, u2);
-
         u1.classList.remove('selected');
         u1.classList.add('inactive');
         u2.classList.remove('selected');
         if (!u2.classList.contains('inactive') && !u2.classList.contains('dead')) {
             u2.classList.add('active');
         }
-
         checkNextPhase();
     }, 500);
-
     unitBase[u1.dataset.count].active = false;
 }
 
 function calculateDamage(u1, u2) {
-    const damage = cheatOn ? 100 : unitBase[u1.dataset.count].damage;
+    const attack = unitBase[u1.dataset.count].damage;
+    const damage = cheatOn ? 100 : Math.floor((Math.random() * hitChance) / 100 * attack);
     const hp = unitBase[u2.dataset.count].hp;
     const name = unitBase[u2.dataset.count].name;
     let currentHp = hp - damage;
     if (currentHp <= 0) {currentHp = 0;}
-
     unitBase[u2.dataset.count].hp = currentHp;
     u2.innerHTML = `${name} ${currentHp}`;
-
+    showDamage(damage, u2);
     checkDeath(u2);
+}
 
+function showDamage(damage, u2) {
+    const damageField = document.createElement('div');
+    damageField.className = 'damage-field';
+    damageField.textContent = `-${damage}`;
+    setTimeout(function() {
+        damageField.textContent = '';
+    }, 1000);
+    u2.appendChild(damageField);
 }
 
 function checkNextPhase() {
-    const aAL = checkActiveUnits();
-    noActivity = aAL == 0 ? true : false;
-   
+    noActivity = checkActiveUnits() == 0 ? true : false;
     if (noActivity) {
         allUnits.forEach(unit => {
             if (!unit.classList.contains('dead')) {
@@ -133,6 +184,7 @@ function checkNextPhase() {
             }
         });
         noActivity = false;
+        // currentTurn = randomSide;
     }
 }
 
@@ -146,6 +198,7 @@ function checkDeath(u2) {
         }
         u2.className = `unit ${unitType} dead`;
     }
+    turnCheck();
     gameOverPanel();
 }
 
@@ -191,7 +244,7 @@ cheatButton.addEventListener('click', ()=> {
 })
 document.body.appendChild(cheatButton);
 
-// information block -------------------------------------------
+// final information block -------------------------------------------
 
 const infoField = document.createElement('div');
 infoField.addEventListener('click', ()=> {
